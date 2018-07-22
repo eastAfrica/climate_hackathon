b86cdba63fd2f7c89061905c7d24f3f8f9ba7c53
NebiyouTen <nebiyoutenager@gmail.com>
2018-07-22 14:16:04 +0200

    removed report Id feedback

--- app/assets/javascripts/districts.js
+++ app/assets/javascripts/districts.js
@@ -1,20 +1,8 @@
 $(document).ready(function(){
-
-  $("#create_report").onclick({
-      let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-      smsSender: function (username) {
-        let xhr = new XMLHttpRequest();
-        let user = username;
-        xhr.open("GET", "https://platform.clickatell.com/messages/http/send?apiKey=Mf1Wwh1VTuy7rE6EFOT_gQ==&to=250734598922&content=" + user + " needs+your+help+Emergency", true);
-        xhr.onreadystatechange = function(){
-          if (xhr.readyState === 4 && xhr.status === 200){
-            console.log('success');
-          }
-        };
-        xhr.send();
-      }
-    
+  $('#playsound').click(function (e) {
+      $('#sound_effect')[0].currentTime = 0;
+      $('#sound_effect')[0].play();
+      return false;
   });
 
 });
---
+++ db/migrate/20180722120825_remove_report_feedback.rb
@@ -0,0 +1,4 @@
+class RemoveReportFeedback < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
